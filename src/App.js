e81ed7f32e7d8cf0c085a8c58ebbc2898d74b1b0
saisubu1997 <saisubu1997@gmail.com>
2019-02-14 12:35:20 +0530

    Signed-off-by: saisubu1997 <saisubu1997@gmail.com>

--- src/App.js
+++ src/App.js
@@ -9,16 +9,11 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+           Served From Local Host <code>src/App.js</code> Gait Line Analysis
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
+
+         
         </header>
       </div>
     );
